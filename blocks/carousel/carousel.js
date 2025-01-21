a7b4b18f6cd0b9f97263db4b743f4ec2bea37991
pankajTCZ <pankaj.datt@techchefz.com>
2025-01-21 17:51:42 +0530

    gtm

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -193,4 +193,20 @@ export default async function decorate(block) {
     bindEvents(block);
     startAutoSlide(block, 5000);
   }
+
+  const gtmScript = document.createElement('script');
+  gtmScript.type = 'text/javascript';
+  gtmScript.async = true;
+  gtmScript.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-MTFGGWH';
+  document.head.appendChild(gtmScript); // Append the GTM script to the head
+
+  const gtmDataLayerScript = document.createElement('script');
+  gtmDataLayerScript.innerHTML = `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MTFGGWH');
+  `;
+  document.head.appendChild(gtmDataLayerScript);
 }
